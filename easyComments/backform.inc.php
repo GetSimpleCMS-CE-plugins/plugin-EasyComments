35c0c739b28c9a10e373c231f5d4a1301b95f0c3
Multi <83913997+multicolor-rgb@users.noreply.github.com>
2024-08-08 15:37:15 +0200

    little fix

--- easyComments/backform.inc.php
+++ easyComments/backform.inc.php
@@ -26,13 +26,13 @@ global $fileLog;
 
 <form method="post">
 
-    <input type="text" style="width:100%;padding:10px;box-sizing:border-box;margin-bottom:5px;" value="<?php echo @file_get_contents(GSDATAOTHERPATH . 'easyCommentsMail.txt'); ?>" placeholder="" name="adminemail">
+    <input type="text" style="width:100%;padding:10px;box-sizing:border-box;margin-bottom:5px;" value="<?php echo @file_get_contents(GSDATAOTHERPATH . 'easyComments/easyCommentsMail.txt'); ?>" placeholder="" name="adminemail">
 
     <a href="https://www.hcaptcha.com/">Grab site key and secret key</a>
     <label style="margin-top:20px;"><?php echo i18n_r('easyComments/SECRETKEY');?></label>
-    <input type="text"  name="secretkey" style="width:100%;padding:10px;box-sizing:border-box;margin-bottom:5px;"  value="<?php echo @file_get_contents(GSDATAOTHERPATH . 'secretkey.txt'); ?>">
+    <input type="text"  name="secretkey" style="width:100%;padding:10px;box-sizing:border-box;margin-bottom:5px;"  value="<?php echo @file_get_contents(GSDATAOTHERPATH . 'easyComments/secretkey.txt'); ?>">
     <label><?php echo i18n_r('easyComments/DOMAINKEY');?></label>
-    <input type="text" name="sitekey" style="width:100%;padding:10px;box-sizing:border-box;margin-bottom:5px;" value="<?php echo @file_get_contents(GSDATAOTHERPATH . 'sitekey.txt'); ?>">
+    <input type="text" name="sitekey" style="width:100%;padding:10px;box-sizing:border-box;margin-bottom:5px;" value="<?php echo @file_get_contents(GSDATAOTHERPATH . 'easyComments/sitekey.txt'); ?>">
 
     <input type="submit" name="saveadminemail" style="border:solid 1px;padding:5px 15px;background:#333;color:#fff;display:inline-block;border-radius:5px;text-decoration:none;margin-bottom:20px;" value="<?php echo i18n('easyComments/SAVEEMAIL'); ?>">
 </form>
